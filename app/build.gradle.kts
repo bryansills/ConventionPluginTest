5bd70d59a8eae8998c9c27db3563c8c1eacc41dd
bryansills <bsills@bignerdranch.com>
2023-07-11 12:16:10 -0500

    initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,37 @@
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    id("ninja.bryansills.android.application")
+}
+
+android {
+    namespace = "ninja.bryansills.conventionplugintest"
+
+    defaultConfig {
+        applicationId = "ninja.bryansills.conventionplugintest"
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+}
+
+dependencies {
+
+    implementation(libs.core.ktx)
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.test.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+}
